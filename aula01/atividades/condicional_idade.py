73dc42c1f997d4a02b688750b429a84058c21763
Alex <60290669+alexprog1@users.noreply.github.com>
2025-05-12 10:06:57 +0000

    Adiciona novas pastas e arquivos Python

---
+++ aula01/atividades/condicional_idade.py
@@ -0,0 +1,8 @@
+# Definimos a idade da pessoa
+idade = 15
+
+# Verificamos se a pessoa é maior de idade
+if idade >= 18:
+    print("Maior de idade")  # Caso tenha 18 anos ou mais
+else:
+    print("Menor de idade")  # Caso tenha menos de 18 anos
